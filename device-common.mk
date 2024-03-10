e585f618c90ff32e1bb090f4d3d72b3b5865c164
Ryan Cueva <51128072+ryancueva282@users.noreply.github.com>
2024-03-09 18:11:43 -0600

    Update device-common.mk for exynos3475 variant

--- device-common.mk
+++ device-common.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from common
-$(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
+$(call inherit-product, device/samsung/exynos3475-common/msm8916.mk)
 
 # Inherit from vendor
 $(call inherit-product-if-exists, vendor/samsung/gte-common/gte-common-vendor.mk)
